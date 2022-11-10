a509762253109e195a8f07a957c3614c8f8d937d
mingi CHOI <bnmnil96@gmail.com>
2022-11-10 15:22:28 +0900

    first.

---
+++ 999_Frontend/src/component/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../css/style.css";
+
+function Footer() {
+  return <div className="footer"></div>;
+}
+
+export default Footer;
---
+++ 999_Frontend/src/component/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../css/style.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <button className="login-btn">Login</button>
+      <button className="signup-btn">Sign-Up</button>
+    </div>
+  );
+}
+
+export default Header;
+
+<div>
+  <div>
+    <input></input>
+    <button></button>
+  </div>
+</div>;
