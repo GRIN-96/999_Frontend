29ed6702ad6ed68dc671349b65268dd0b7f78a94
cgy0627 <jennygychae@gmail.com>
2022-11-11 21:46:18 +0900

    new post page update

--- 999_Frontend/src/components/NewPost_Body.js
+++ 999_Frontend/src/components/NewPost_Body.js
@@ -9,19 +9,6 @@ import Post_section from "./Post_section";
 import Box3 from "./Box3";
 
 function Body() {
-  const userList = [
-    {
-      userId: "456@naver.com",
-      postContents: "두 번째 게시글 입니다.",
-      postDate: "2022-11-11 09:47:19",
-    },
-    {
-      userId: "789@naver.com",
-      postContents: "세 번째 게시글 입니다.",
-      postDate: "2022-11-11 09:47:19",
-    },
-  ];
-
   // User data 가져오기
   // useEffect는 페이지가 실행되면 가장 먼저 실행되는 부분!!
   useEffect(() => {
@@ -38,7 +25,6 @@ function Body() {
   const onLogin = () => {
     setLogin(!login);
   };
-
   return (
     // body 를 구성하는 3개의 박스를 하나의 div (id="wrap1")으로 감쌌다.
     <div id="wrap1">
@@ -62,17 +48,28 @@ function Body() {
           HOME
         </button>
       </div>
-      <div className="box2">
-        <div>
-          {userList.map((user) => (
-            <Post_section
-              n_name={user.userId}
-              content={user.postContents}
-              date={user.postDate}
-            />
-          ))}
-        </div>
+      {/* ------------------------box2 수정------------------------ */}
+      <div className="newpost_box2">
+        <section className="newpost_section_view">
+          <h2 className="newpost_title">Write new content</h2>
+          <div>
+            <input
+              type="text"
+              className="contentInput"
+              placeholder="Write content..."
+            ></input>
+          </div>
+          <button
+            className="add_newPost"
+            onClick={() => {
+              document.location.href = "/Main";
+            }}
+          >
+            Submit
+          </button>
+        </section>
       </div>
+      {/* ------------------------box3 수정------------------------ */}
       <div className="box3">
         <img className="my_profile" src={mingi} alt="profile"></img>
         <div className="profile_info">
@@ -101,14 +98,6 @@ function Body() {
             Post #: {user.userPostCnt}
           </p>
         </div>
-        <button
-          className="add_posts"
-          onClick={() => {
-            document.location.href = "/NewPost";
-          }}
-        >
-          +
-        </button>
       </div>
     </div>
   );
