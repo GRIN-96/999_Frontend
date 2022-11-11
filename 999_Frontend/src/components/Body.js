7e8a89e5d0bc1c6d8e39a7b6826854865391ffdc
mingi CHOI <bnmnil96@gmail.com>
2022-11-11 21:47:04 +0900

    post fix

--- 999_Frontend/src/components/Body.js
+++ 999_Frontend/src/components/Body.js
@@ -37,7 +37,7 @@ function Body() {
   }, []);
 
   const [user, setUser] = useState({});
-  console.log(user);
+  const img = user.userMbti;
 
   return (
     // body 를 구성하는 3개의 박스를 하나의 div (id="wrap1")으로 감쌌다.
@@ -66,13 +66,17 @@ function Body() {
         <div>
           {userList.map((user) => (
             <Post_section
+              // key={}
               n_name={user.userId}
               content={user.postContents}
               date={user.postDate}
+              img={img}
             />
           ))}
         </div>
       </div>
+
+      {/* collection view 중 3번째에 해당하는 부분이다. */}
       <div className="box3">
         <img className="my_profile" src={mingi} alt="profile"></img>
         <div className="profile_info">
@@ -100,6 +104,8 @@ function Body() {
             ></img>
             Post #: {user.userPostCnt}
           </p>
+
+          <button className="add_posts">+</button>
         </div>
       </div>
     </div>
--- 999_Frontend/src/components/Post_section.js
+++ 999_Frontend/src/components/Post_section.js
@@ -1,12 +1,18 @@
 import React from "react";
-import gugu from "../img/gugu.png";
-import mingi from "../img/mingi.jpg";
-import img1 from "../img/sleep.jpeg";
+import img5 from "../img/gugu.png";
+import img1 from "../img/강아쥐.jpg";
+import img2 from "../img/멋쟁이최맹기.jpg";
+import img3 from "../img/본호번호볼콕.jpeg";
+import img4 from "../img/팬더엉덩이.jpg";
+import img6 from "../img/홀쭉본호.jpeg";
 
-function Post_section({ n_name, content, date }) {
+function Post_section({ n_name, content, date, img }) {
+  const imgs = [img1, img2, img3, img4, img5, img6];
+  const idx = Math.floor(Math.random() * 6);
+  console.log(n_name, content, date, img);
   return (
     <section className="section_view">
-      <img className="profile_img" src={mingi} width="100px" height="100px" />
+      <img className="profile_img" src={img} width="100px" height="100px" />
       <div className="posts">
         <div className="post_name">
           <span>{n_name}</span>
@@ -14,7 +20,7 @@ function Post_section({ n_name, content, date }) {
           <span className="post_detail">{date}</span>
         </div>
         <div className="post_content">{content}</div>
-        <img className="post_img" src={img1} />
+        <img className="post_img" src={imgs[idx]} />
       </div>
     </section>
   );
