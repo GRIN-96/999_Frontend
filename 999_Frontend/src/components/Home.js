ac54dd97a3d922492f2a5d3ef149c526fc0d150f
mingi CHOI <bnmnil96@gmail.com>
2022-11-11 19:06:09 +0900

    누나 받아주세요 git status!

--- 999_Frontend/src/components/Home.js
+++ 999_Frontend/src/components/Home.js
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <div>
       <h1>HOME</h1>
-      {/* <Link to="/Login">Login</Link> <br /> */}
       <Link to="/Main">Main Page</Link> &nbsp;
       <Link to="/ListUser">User List</Link> &nbsp;
     </div>
