a7e9b95d1468f25b83e442791e462cece1e59ead
mingi CHOI <bnmnil96@gmail.com>
2022-11-11 14:13:26 +0900

    section

--- 999_Frontend/src/components/Body.js
+++ 999_Frontend/src/components/Body.js
@@ -1,45 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { axiosData } from "../api/Users";
 import "../css/body.css";
 import gugu from "../img/gugu.png";
 import mingi from "../img/mingi.jpg";
 import img1 from "../img/sleep.jpeg";
+import Post_section from "./Post_section";
 
 const name = "name";
 const email = "email@email.com";
 const postNum = 0;
 const content =
-  "contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent";
-const nickname = "@mingi";
-const date = "1시간";
+  "contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent";
 
 function Body() {
+  const userList = [
+    {
+      userId: "456@naver.com",
+      postContents: "두 번째 게시글 입니다.",
+      postDate: "2022-11-11 09:47:19",
+    },
+    {
+      userId: "789@naver.com",
+      postContents: "세 번째 게시글 입니다.",
+      postDate: "2022-11-11 09:47:19",
+    },
+  ];
+
+  // User data 가져오기
+  // useEffect는 페이지가 실행되면 가장 먼저 실행되는 부분!!
+  useEffect(() => {
+    const data = axiosData();
+
+    data.then((data) => setUser(JSON.parse(data.user)));
+  }, []);
+
+  const [user, setUser] = useState({});
+  console.log(user);
+
   return (
-    <body id="wrap1">
+    // body 를 구성하는 3개의 박스를 하나의 div (id="wrap1")으로 감쌌다.
+    <div id="wrap1">
       <div className="box1">
         <div className="icon">
           <img src={gugu} width="100px" height="100px" />
         </div>
       </div>
       <div className="box2">
-        <script>
-          <section className="section_view">
-            <img
-              className="profile_img"
-              src={mingi}
-              width="100px"
-              height="100px"
+        <div>
+          {userList.map((user) => (
+            <Post_section
+              n_name={user.userId}
+              content={user.postContents}
+              date={user.postDate}
             />
-            <div className="posts">
-              <div className="post_name">
-                <span>{nickname}</span>
-                <span className="post_detail">{nickname}</span>
-                <span className="post_detail">{date}</span>
-              </div>
-              <div className="post_content">{content}</div>
-              <img className="post_img" src={img1} />
-            </div>
-          </section>
-        </script>
+          ))}
+        </div>
       </div>
       <div className="box3">
         <img className="my_profile" src={mingi} alt="profile"></img>
@@ -70,7 +85,7 @@ function Body() {
           </p>
         </div>
       </div>
-    </body>
+    </div>
   );
 }
 
